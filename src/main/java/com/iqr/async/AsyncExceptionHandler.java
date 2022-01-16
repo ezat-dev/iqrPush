81908e652d813fa10fe98a7d687ebcade68bc2cf
Ko HyeonJin <hjko@ezat.co.kr>
2022-01-16 20:37:23 +0900

    서버 적용 전

--- src/main/java/com/iqr/async/AsyncExceptionHandler.java
+++ src/main/java/com/iqr/async/AsyncExceptionHandler.java
@@ -8,11 +8,11 @@ public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler{
 
 	@Override
 	public void handleUncaughtException(Throwable throwable, Method method, Object... obj) {
-		System.out.println("Thread Error Exception");
-		System.out.println("Exception Message :: " + throwable.getMessage());
-		System.out.println("Method name :: " + method.getName());
+//		System.out.println("Thread Error Exception");
+//		System.out.println("Exception Message :: " + throwable.getMessage());
+//		System.out.println("Method name :: " + method.getName());
 			for(Object param : obj) {
-				System.out.println("Param Val ::: " + param);
+//				System.out.println("Param Val ::: " + param);
 			}
 	}
 
--- src/main/java/com/iqr/async/LogProcessor.java
+++ src/main/java/com/iqr/async/LogProcessor.java
@@ -7,8 +7,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import com.iqr.domain.Alarms;
-import com.iqr.domain.Pushs;
 import com.iqr.domain.Users;
 import com.iqr.service.AlarmService;
 import com.iqr.service.UserService;
@@ -22,8 +20,9 @@ public class LogProcessor {
 	@Autowired
 	private UserService userService;
 	
-	//주기별 실행 60000 = 1분
-	@Scheduled(fixedRate = 60000)
+	//주기별 실행 60000 = 1분 / 10분 변경 fixedRate = 600000
+	//월-금 오전 9시
+	@Scheduled(cron="0 0 9 ? * MON-FRI")
 	public void handle() throws InterruptedException {
 //		System.out.println("11");
 		FcmUtil fcmUtil = new FcmUtil();
@@ -62,16 +61,20 @@ public class LogProcessor {
 					//발생한 알람을 받을 사람이 있는지 체크
 					tokenList = userService.getUserTokenList(alarmList.get(i).getU_company());	
 					
-					if(tokenList.size() != 0) {					
-						Map<String, Object> param = new HashMap<String, Object>();
-						param.put("qr_code", alarmList.get(i).getQr_code());
-						param.put("alarm_name",alarmList.get(i).getAlarm_name());
-						param.put("alarm_time",alarmList.get(i).getAlarm_time());
-						param.put("alarm_sdate", alarmList.get(i).getAlarm_sdate());
-						alarmService.pushAlarmRegist(param);
-						
+					if(tokenList.size() != 0) {
 						//아래 메소드는 사용자 테이블에 token_id 업데이트 한 다음 테스트
-					
+
+						if(j == 0) {
+							Map<String, Object> param = new HashMap<String, Object>();
+							param.put("qr_code", alarmList.get(i).getQr_code());
+							param.put("alarm_name",alarmList.get(i).getAlarm_name());
+							param.put("alarm_time",alarmList.get(i).getAlarm_time());
+							param.put("alarm_sdate", alarmList.get(i).getAlarm_sdate());
+							
+							alarmService.pushAlarmRegist(param);
+							
+						}
+						
 						fcmUtil.corr_FCM(tokenList, 
 								alarmList.get(i).getAlarm_time(),
 								alarmList.get(i).getAlarm_name(),
